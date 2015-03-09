d45420ee50da8bf3973cfb47ab98b0a9dce9f60e
kingrichard2005@gmail.com <kingrichard2005@gmail.com>
2015-03-08 18:30:29 -0700

    Updating based on latest assignment req. updates

--- a2.py
+++ a2.py
@@ -31,7 +31,7 @@ class TestHelper():
       except:
          print "error in writeABunchOfNumbersToAFile(...)"
 
-class DataManager():
+class Homework2():
     # Class to manage input data
     def __init__( self ):
         self.TwoD           = np.array([[]]);
@@ -44,24 +44,43 @@ class DataManager():
         except:
             raise Exception("error in method1LoadAFileToTwoD(...)");
 
-    def extractSectionOfTwoDArray( self, rowIndexesToExtract = [], colIndexesToExtract = [] ):
-       # Extract a section of the DataManager's internal 2D numpy array using a row/column 
-       # filter mask
+    def get_ten_by_ten_array(self):
         try:
-           rowAxisNdx      = 0;
-           columnAxisNdx   = 1;
-           # Build a row filter mask
-           row_filter_mask = np.array( [True if i in rowIndexesToExtract else False for i in range(0, int(self.TwoD.shape[rowAxisNdx]))] );
-           # Build a column filter mask
-           col_filter_mask = np.array( [True if i in colIndexesToExtract else False for i in range(0, int(self.TwoD.shape[columnAxisNdx]))] );
+            pass;
+        except:
+            raise Exception ("error in get_ten_by_ten_array()");
 
-           # split
-           subSection          = self.TwoD[ row_filter_mask ];
-           self.extractedArray = subSection[:,col_filter_mask];
+    def get_selected_row_indicies(self, selected_row_indicies = [], section = []):
+        try:
+            rowAxisNdx      = 0;
+            # Build a row filter mask
+            row_filter_mask = np.array( [True if i in selected_row_indicies else False for i in range(0, int(section.shape[rowAxisNdx]))] );
+            return section[ row_filter_mask ];
+        except:
+            raise Exception ("error in get_selected_row_indicies(...)");
+
+    def get_selected_column_indicies(self, selected_column_indicies = [], section = []):
+        try:
+            columnAxisNdx   = 1;
+            # Build a column filter mask
+            col_filter_mask = np.array( [True if i in selected_column_indicies else False for i in range(0, int(self.TwoD.shape[columnAxisNdx]))] );
+            return section[:,col_filter_mask];
+        except:
+            raise Exception ("error in get_selected_column_indicies(...)");
+
+    def get_subarray( self, selected_row_indicies = [], selected_column_indicies = [] ):
+        '''
+           Extract a section of the DataManager's internal 2D numpy array using a row/column 
+           filter mask.
+        '''
+        try:
+           section             = self.get_selected_row_indicies( selected_row_indicies, self.TwoD );
+           self.extractedArray = self.get_selected_column_indicies(selected_column_indicies, section = section)
+           return self.extractedArray;
         except:
             raise Exception("error in extractSectionOfArray(...)");
 
-    def normalizeTwoDArray( self, a = 1.0, b = 0.0, normAxis = 0 ):
+    def get_normalized_array( self, a = 1.0, b = 0.0, normAxis = 0 ):
         '''
         Normalizes numpy array values to be between arbitrary points 'a' and 'b' 
         along specified 'normaxis' using generalized feature scaling method.
@@ -91,6 +110,40 @@ class DataManager():
         except:
             raise Exception("error in normalizeTwoDArray(...)");
 
+    def get_mean(self, array):
+        '''
+            This function gets the array and returns the mean of all elements
+            You must use the built_in function of mean to do this question
+        '''
+        try:
+            theMean = np.mean(array);
+            return theMean;
+        except:
+            raise Exception("");
+
+    def get_ten_by_ten_median(self, array):
+        '''
+            This function gets the array and returns the median of all elements
+            You must use the built_in function of median to do this question
+        '''
+        try:
+            theMedian = np.median(self.TwoD);
+            return theMedian;
+        except:
+            raise Exception("error in get_ten_by_ten_median");
+
+
+    def get_ten_by_ten_standard_deviation(self, array):
+        '''
+            This function gets the array and returns the standard deviation of all elements
+            You must use the built_in function of standard deviation to do this question
+        '''
+        try:
+            theStdDev = np.std(array);
+            return theStdDev;
+        except:
+            raise Exception("error in get_ten_by_ten_standard_deviation");
+
 class TestUM(unittest.TestCase):
     # Data Manager tester
     def setUp(self):
@@ -98,47 +151,84 @@ class TestUM(unittest.TestCase):
         # a file with a bunch of integers
         self.helper = TestHelper();
  
-    def test_load_a_file(self):
-        # Arrange: Create a DataMananger
-        dataManager = DataManager();
-        # Act: Read
-        dataManager.readArrayFromFile(self.helper.aTempFile); 
-        # Assert: The loaded array matches the 10 x 10 array read from a file
-        expected    = self.helper.a2dArr;
-        result      = dataManager.TwoD;
+    def test_get_ten_by_ten(self):
+        # Arrange: Create a Homework2 class object
+        homework2 = Homework2();
+        # Act: get 10 x 10 array
+        homework2.readArrayFromFile(self.helper.aTempFile); 
+        # Assert: 10 x 10 array expected
+        expected = np.array([[ 0,  1,  2,  3,  4,  5,  6, 7,  8,  9],
+                     [10, 11, 12, 13, 14, 15, 16,17, 18, 19],
+                     [20, 21, 22, 23, 24, 25, 26,27, 28, 29],
+                     [30, 31, 32, 33, 34, 35, 36,37, 38, 39],
+                     [40, 41, 42, 43, 44, 45, 46,47, 48, 49],
+                     [50, 51, 52, 53, 54, 55, 56,57, 58, 59],
+                     [60, 61, 62, 63, 64, 65, 66,67, 68, 69],
+                     [70, 71, 72, 73, 74, 75, 76,77, 78, 79],
+                     [80, 81, 82, 83, 84, 85, 86,87, 88, 89],
+                     [90, 91, 92, 93, 94, 95, 96,97, 98, 99]])
+        result      = homework2.TwoD;
         self.assertEqual( np.array_equal(expected,result), True );
 
     def test_extracted_portion_matches_dimensions(self):
         # Arrange: Create a DataMananger with loaded data
-        dataManager = DataManager();
+        dataManager = Homework2();
         dataManager.readArrayFromFile(self.helper.aTempFile); 
         # Act: Extract a section of the array
         rowIndexesToExtract = [3, 5, 7, 9];
         colIndexesToExtract = [2, 4, 6, 8];
-        dataManager.extractSectionOfTwoDArray(rowIndexesToExtract, colIndexesToExtract);
+        result = dataManager.get_subarray(rowIndexesToExtract, colIndexesToExtract);
         # Assert: Extracted section matches expected array dimensions
         expected    = (4,4);
-        result      = dataManager.extractedArray.shape;
+        result      = result.shape;
         self.assertEqual( expected,result );
 
-    def test_twod_normalization(self):
+    def test_get_selected_row_indidicies(self):
+        # Arrange: Create a DataMananger with loaded data
+        dataManager = Homework2();
+        dataManager.readArrayFromFile(self.helper.aTempFile); 
+        #Act: Get selected row indicies
+        rowIndexesToExtract = [3, 5, 7, 9];
+        result = dataManager.get_selected_row_indicies(selected_row_indicies = rowIndexesToExtract, section = dataManager.TwoD);
+        # Assert: Extracted section matches expected array dimensions
+        expected    = (4,10);
+        result      = result.shape;
+        self.assertEqual( expected,result );
+
+    def test_get_selected_col_indidicies(self):
+        # Arrange: Create a DataMananger with loaded data
+        dataManager = Homework2();
+        dataManager.readArrayFromFile(self.helper.aTempFile); 
+        #Act: Get selected column indicies
+        rowIndexesToExtract = [3, 5, 7, 9];
+        colIndexesToExtract = [2, 4, 6, 8];
+        rowSection          = dataManager.get_selected_row_indicies(selected_row_indicies = rowIndexesToExtract, section = dataManager.TwoD);
+        colsection          = dataManager.get_selected_column_indicies( selected_column_indicies = colIndexesToExtract, section = rowSection );
+        # Assert: Extracted section matches expected array dimensions
+        expected    = (4,4);
+        result      = colsection.shape;
+        self.assertEqual( expected,result );
+
+    def test_normalize_between_0_1(self):
         # Arrange: Create a DataMananger with loaded data and extracted subarray
-        dataManager = DataManager();
+        dataManager = Homework2();
         dataManager.readArrayFromFile(self.helper.aTempFile); 
         rowIndexesToExtract = [3, 5, 7, 9];
         colIndexesToExtract = [2, 4, 6, 8];
-        dataManager.extractSectionOfTwoDArray(rowIndexesToExtract, colIndexesToExtract);
+        dataManager.get_subarray(rowIndexesToExtract, colIndexesToExtract);
         # Act: Normalize extracted array
-        dataManager.normalizeTwoDArray();
+        a = 1.0;
+        b = 0.0;
+        dataManager.get_normalized_array(a = a, b = b);
         # Assert: All normalized values are between 0 and 1
         expected    = True;
-        result      = [ True if (0.0 <= i <= 1.0) else False for i in np.ravel(dataManager.normalizedTwoD)];
+        result      = [ True if (b <= i <= a) else False for i in np.ravel(dataManager.normalizedTwoD)];
         result      = True if (result.count(False) == 0) else False;
         self.assertEqual( expected,result );
 
     def test_mean_median_and_standarddeviation_of_2dArray(self):
         # Arrange: Create a DataMananger with loaded data
-        dataManager = DataManager();
+        dataManager = Homework2();
         dataManager.readArrayFromFile(self.helper.aTempFile); 
         # Act: Find the mean, median and standard deviation of loaded two dimensional array
         statsDict   = dataManager.computeBasicStats();
